f9d73af9f287a233ebe13074fe1d36e4ecc9e8ad
Patrick Harding <hardig_pm@live.com>
2015-04-09 13:30:29 -0600

    updating my gitignore for pyc files

---
+++ backend/apps/recipes/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from models import Recipe
+admin.site.register(Recipe)
---
+++ backend/apps/recipes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    directions = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
