58602e4978e7660c99c0a5a0ad9540de635329b9
Patrick Harding <hardig_pm@live.com>
2015-04-10 16:23:04 -0600

    new Angular files

--- backend/apps/recipes/models.py
+++ backend/apps/recipes/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-
+# this defines what you want your database to look like
+# each class in this represents a table in our database
+# the columns are represented by the name, description, directions
+# each row is an instantiation of the class
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
